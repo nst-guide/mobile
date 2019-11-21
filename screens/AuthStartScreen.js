08261c432a1455d887cd83c20c1dfba6fbc501c1
Kyle Barron <barronk@mit.edu>
2019-11-20 22:45:10 -0700

    Fix eslint and prettier errors

--- screens/AuthStartScreen.js
+++ screens/AuthStartScreen.js
@@ -1,10 +1,10 @@
-import React, { memo } from "react";
-import Background from "../components/Background";
-import Logo from "../components/Logo";
-import Header from "../components/Header";
-import Button from "../components/Button";
-import BackButton from "../components/BackButton";
-import Paragraph from "../components/Paragraph";
+import React, { memo } from 'react';
+import Background from '../components/Background';
+import Logo from '../components/Logo';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import BackButton from '../components/BackButton';
+import Paragraph from '../components/Paragraph';
 
 const HomeScreen = ({ navigation }) => (
   <Background>
@@ -16,10 +16,10 @@ const HomeScreen = ({ navigation }) => (
     <Paragraph>
       The easiest way to start with your amazing application.
     </Paragraph>
-    <Button mode="contained" onPress={() => navigation.navigate("Login")}>
+    <Button mode="contained" onPress={() => navigation.navigate('Login')}>
       Login
     </Button>
-    <Button mode="outlined" onPress={() => navigation.navigate("Register")}>
+    <Button mode="outlined" onPress={() => navigation.navigate('Register')}>
       Sign Up
     </Button>
   </Background>
