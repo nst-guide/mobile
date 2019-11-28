f7367755ef356692521a845778fe421694cbcf30
Kyle Barron <barronk@mit.edu>
2019-11-28 02:02:24 -0700

    Reformat code using prettier

--- core/utils.js
+++ core/utils.js
@@ -1,4 +1,4 @@
-export const emailValidator = (email) => {
+export const emailValidator = email => {
   const re = /\S+@\S+\.\S+/;
 
   if (!email || email.length <= 0) return 'Email cannot be empty.';
@@ -7,19 +7,19 @@ export const emailValidator = (email) => {
   return '';
 };
 
-export const passwordValidator = (password) => {
+export const passwordValidator = password => {
   if (!password || password.length <= 0) return 'Password cannot be empty.';
   if (password.length < 8) return 'Password must be at least 8 characters.';
 
   return '';
 };
 
-export const usernameValidator = (username) => {
+export const usernameValidator = username => {
   const validSymbols = /^[a-zA-Z0-9-_~]+$/;
 
   if (!username || username.length <= 0) return 'Username cannot be empty.';
   if (!validSymbols.test(username)) {
-    return 'Username can contain only a-z, A-Z, 0-9, -, ~, and _.'
+    return 'Username can contain only a-z, A-Z, 0-9, -, ~, and _.';
   }
 
   return '';
